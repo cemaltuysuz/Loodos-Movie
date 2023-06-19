177287ecd2a35400ce0fab95987d7fb969afc65d
Cemal Tüysüz <cemal.tuysuz@topkapidanismanlik.com.tr>
2023-06-19 16:11:33 +0300

    + dependencies + folder structure

--- Loodos-Movie/Loodos-Movie/Flows/Splash/SplashVC.swift
+++ Loodos-Movie/Loodos-Movie/Flows/Splash/SplashVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SplashVC.swift
 //  Loodos-Movie
 //
 //  Created by Cemal Tuysuz on 19.06.2023.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SplashVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         view.backgroundColor = .red
     }
 }
-
